b728d66e37ccb6c60dc7f1b79ceaae11cf8d22a3
mingda.cai <mingda.cai@valid.com>
2020-04-22 16:36:17 +0800

    [release] InputDataGeneration_v1.3
    1, give option to calculate luhn or not;
    2, make imsi, msisdn optional;
    3, add delimiter text field;

--- Java/InputDataGeneration/src/inputDataGeneration/InputDataGeneration.java
+++ Java/InputDataGeneration/src/inputDataGeneration/InputDataGeneration.java
@@ -18,11 +18,13 @@ import javax.swing.filechooser.FileSystemView;
  */
 public class InputDataGeneration implements  ActionListener{
 
-	String version = "v1.1";
+	String version = "v1.3";
 	
 	JTextField path = new JTextField("");
 	JTextField number = new JTextField("");
+	JTextField delimiter = new JTextField("");
 	JTextField iccid = new JTextField("");
+	JCheckBox luhnCalcCheck = new JCheckBox("Luhn Calc");
 	JTextField imsi = new JTextField("");
 	JTextField msisdn = new JTextField("");
 	JButton button = new JButton();
@@ -33,20 +35,22 @@ public class InputDataGeneration implements  ActionListener{
 		JPanel panel = new JPanel();
 		JLabel pathLabel = new JLabel("Path:");
 		JLabel numberLabel = new JLabel("Number:");
+		JLabel delimiterLabel = new JLabel("Delimiter:");
 		JLabel iccidLabel = new JLabel("ICCID col:");
-		JLabel imsiLabel = new JLabel("IMSI col:");
-		JLabel msisdnLabel = new JLabel("MSISDN col:");
+		JLabel imsiLabel = new JLabel("IMSI col(Optional):");
+		JLabel msisdnLabel = new JLabel("MSISDN col(Optional):");
 		GridBagConstraints c = new GridBagConstraints();
 		
 		frame.add(panel, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("InputDataGeneration " + version);
-		frame.setSize(550, 350);
+		frame.setSize(650, 400);
 		frame.setResizable(false);
 		frame.setVisible(true);
 		
 		path.setColumns(20);
 		number.setColumns(5);
+		delimiter.setColumns(5);
 		iccid.setColumns(5);
 		imsi.setColumns(5);
 		msisdn.setColumns(5);
@@ -81,24 +85,33 @@ public class InputDataGeneration implements  ActionListener{
 		panel.add(number, c);
 		c.gridx = 0;
 		c.gridy = 2;
-		panel.add(iccidLabel, c);
+		panel.add(delimiterLabel, c);
 		c.gridx = 1;
 		c.gridy = 2;
-		panel.add(iccid, c);
+		panel.add(delimiter,c);
 		c.gridx = 0;
 		c.gridy = 3;
+		panel.add(iccidLabel, c);
+		c.gridx = 1;
+		c.gridy = 3;
+		panel.add(iccid, c);
+		c.gridx = 2;
+		c.gridy = 3;
+		panel.add(luhnCalcCheck);
+		c.gridx = 0;
+		c.gridy = 4;
 		panel.add(imsiLabel, c);
 		c.gridx = 1;
-		c.gridy = 3;
+		c.gridy = 4;
 		panel.add(imsi, c);
 		c.gridx = 0;
-		c.gridy = 4;
+		c.gridy = 5;
 		panel.add(msisdnLabel, c);
 		c.gridx = 1;
-		c.gridy = 4;
+		c.gridy = 5;
 		panel.add(msisdn, c);
 		c.gridx = 0;
-		c.gridy = 5;
+		c.gridy = 6;
 		panel.add(button, c);
 		
 
@@ -126,11 +139,11 @@ public class InputDataGeneration implements  ActionListener{
 	private void dataGeneration() {
 		File fin = new File(path.getText());
 		File fout = new File(path.getText()+"_Generated");
-
+		
 		try {
 			Integer iccidCol = Integer.parseInt(iccid.getText());
-			Integer imsiCol = Integer.parseInt(imsi.getText());
-			Integer msisdnCol = Integer.parseInt(msisdn.getText());
+			Integer imsiCol = (!imsi.getText().isEmpty())?Integer.parseInt(imsi.getText()):0;
+			Integer msisdnCol = (!msisdn.getText().isEmpty())?Integer.parseInt(msisdn.getText()):0;
 			
 			FileReader fr = new FileReader(fin);
 			BufferedReader br = new BufferedReader(fr);
@@ -139,7 +152,7 @@ public class InputDataGeneration implements  ActionListener{
 			String line;
 			while((line = br.readLine()) != null) {
 				if(line.length()>1 && Character.isDigit(line.charAt(0)) && Character.isDigit(line.charAt(1))) {
-					String[] data = line.split(" ");
+					String[] data = line.split(delimiter.getText());
 					
 					for(int i = 0;i < data.length; ++i) {
 						dataMap.put(i+1, data[i]);
@@ -155,12 +168,29 @@ public class InputDataGeneration implements  ActionListener{
 			
 			for(int i = Integer.parseInt(number.getText()); i > 0; --i) {
 				String strIccid = dataMap.get(iccidCol);
-				String strImsi = dataMap.get(imsiCol);
-				String strMsisdn = dataMap.get(msisdnCol);
-				dataMap.put(iccidCol, MyUtils.appendLuhnBit(Long.toString(Long.parseLong(strIccid.substring(0, strIccid.length()-1)) + 1)));
-				dataMap.put(imsiCol, Long.toString(Long.parseLong(strImsi) + 1));
-				dataMap.put(msisdnCol, Long.toString(Long.parseLong(strMsisdn) + 1));
-				String data = dataMap.entrySet().stream().map(entry -> entry.getValue() + "").collect(Collectors.joining(" "));
+				String strImsi = "";
+				String strMsisdn = "";
+				if(!imsi.getText().isEmpty()) {
+					strImsi = dataMap.get(imsiCol);
+				}
+				if(!msisdn.getText().isEmpty()) {
+					strMsisdn = dataMap.get(msisdnCol);
+				}
+				String iccid = "";
+				if(luhnCalcCheck.isSelected()) {
+					iccid = MyUtils.appendLuhnBit(Long.toString(Long.parseLong(strIccid.substring(0, strIccid.length()-1)) + 1));
+				}
+				else {
+					iccid = Long.toString(Long.parseLong(strIccid) + 1);
+				}
+				dataMap.put(iccidCol, iccid);
+				if(!imsi.getText().isEmpty()) {
+					dataMap.put(imsiCol, Long.toString(Long.parseLong(strImsi) + 1));
+				}
+				if(!msisdn.getText().isEmpty()) {
+					dataMap.put(msisdnCol, Long.toString(Long.parseLong(strMsisdn) + 1));
+				}
+				String data = dataMap.entrySet().stream().map(entry -> entry.getValue() + "").collect(Collectors.joining(delimiter.getText()));
 				bw.write(data);
 				bw.newLine();
 			}
